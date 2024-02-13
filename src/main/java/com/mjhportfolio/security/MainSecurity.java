6ee9d1ab572df7d736935692f8f6a6ca206d3bec
joako-herrero <joaquinherrero13@gmail.com>
2024-02-13 18:27:40 -0300

    permitiendo explicitamente el httpMethod Options para /email/send

--- src/main/java/com/mjhportfolio/security/MainSecurity.java
+++ src/main/java/com/mjhportfolio/security/MainSecurity.java
@@ -4,6 +4,7 @@ package com.mjhportfolio.security;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,9 +32,11 @@ public class MainSecurity {
              .csrf(csrf -> csrf.disable())
              .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
              .authorizeHttpRequests((authorize) -> authorize
+             .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.OPTIONS,"/email/send")).permitAll()
              .requestMatchers(AntPathRequestMatcher.antMatcher("/email/send")).permitAll()
+             .anyRequest().authenticated()
              );
-          
+            
 
             return http.build();
     }
@@ -43,7 +46,7 @@ public class MainSecurity {
         System.out.println("Setting up CORS configuration...");
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         
-        corsConfiguration.setAllowedOrigins(List.of("**"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("POST", "OPTIONS"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(List.of("*"));
